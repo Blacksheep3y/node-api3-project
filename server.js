8fdd0d8d9d75e2a68cf9daed334e74bc1bb75613
Matt C <Blacksheep3y@gmail.com>
2020-08-13 19:21:35 -0400

    MVP met, but not able to properly test in Postman

--- server.js
+++ server.js
@@ -5,12 +5,18 @@ const server = express();
 const userRouter = require('./users/userRouter');
 const postRouter = require('./posts/postRouter');
 // middleware
-const logger = require("./middleware/logger")
+const logger = require("./logger/logger")
 // server.use
 server.use(express.json());
 server.use(logger());
 server.use("/api/users", userRouter);
 server.use("/api/posts", postRouter)
+server.use((err, req, res, next) => {
+  console.log(err)
+  res.status(500).json({
+    message: "Something went wrong, try again later",
+  })
+})
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
